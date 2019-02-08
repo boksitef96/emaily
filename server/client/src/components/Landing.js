082368d8efbc7376bf5068e5d388c6386ee68ac8
unknown <bokistef96@yahoo.com>
2019-02-08 01:31:03 +0100

    working on react client side

---
+++ server/client/src/components/Landing.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Landing extends Component {
+  render () {
+    return (
+      <div style={{textAlign: 'center'}}>
+        <h2>
+          Emaily!
+        </h2>
+        <h5>Web application for collecting feedback from your users</h5>
+      </div>
+    );
+  }
+}
+
+export default Landing;
